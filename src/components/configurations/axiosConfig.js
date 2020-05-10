57a473aebba360ef8078e5018cb1ebc1bb888d5e
fixmylifedesigns <ijd.irving@gmail.com>
2020-05-09 20:05:07 -0400

    localhost

--- src/components/configurations/axiosConfig.js
+++ src/components/configurations/axiosConfig.js
@@ -1,14 +1,26 @@
 import axios from "axios";
 
+// function axiosWithAuth() {
+
+//   return axios.create({
+//     baseURL: process.env.REACT_APP_DOMAIN_NAME,
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Access-Control-Allow-Credentials": true,
+//     },
+//     withCredentials: true,
+//   });
+// }
+
 function axiosWithAuth() {
+  const token = localStorage.getItem('token');
 
   return axios.create({
     baseURL: process.env.REACT_APP_DOMAIN_NAME,
     headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Credentials": true,
-    },
-    withCredentials: true,
+      'Content-Type': 'application/json',
+      'Authorization': `${token}`,
+  },
   });
 }
 
